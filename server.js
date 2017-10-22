8639424b36a7906942c25504d3a5609d85fab4f5
Dan Reeves <hey@danreev.es>
2017-10-22 12:40:37 +0100

    🛀🐑 Updated with Glitch (#2)

--- server.js
+++ server.js
@@ -1,75 +1,31 @@
-const { ReadableStreamBuffer } = require('stream-buffers');
-const Eris = require('eris');
+const bot = require('discord.js').Client;
 const SpeechService = require('ms-bing-speech-service');
 const { last } = require('lodash');
 
-const bot = new Eris(process.env.DISCORD_BOT_TOKEN);
 const recogniser = new SpeechService({
   language: 'en-US',
   subscriptionKey: process.env.BING_SPEECH_API_KEY
 });
 
-bot.on('ready', () => {
-    console.log('🤖 Ready');
+bot.on('ready', () => console.log('🤖 Ready'));
 
-    const server = last(bot.guilds.filter(g => g.name === 'MSN Messenger')); // This is the name of my Discord server, 
-                                                                             // because bots can be members of multiple "guilds"
-                                                                             // You should change this to the name of your server
+bot.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
 
-    const voiceChannel = last(server.channels.filter(c => c.name === 'MSN Notification')); // This is the name of the channel I want the bot to join
-
-  
-    bot.joinVoiceChannel(voiceChannel.id).then(voiceConnection => {
-        console.log('📞 Joined', voiceChannel.name);
-        
-        // Log errors
-        voiceConnection.on('error', console.log.bind(console));
-        // Log warnings
-        voiceConnection.on('warn', console.log.bind(console));
-        // Log info
-        voiceConnection.on('debug', console.log.bind(console));
-      
-        voiceConnection.setVolume(1);
-      
-        // This is our audio connection to discord
-        // It's an EventEmitter that emits data events containing a Buffer of audio
-        const audioEmitter = voiceConnection.receive('pcm');
-      
-        let audioStream = new ReadableStreamBuffer({
-          frequency: 960,   // in milliseconds.
-          chunkSize: 3840   // in bytes.
-        });
-      
-        audioEmitter.on('data', (...args) => {
-          const [ data, userID, timestamp, sequence ] = args;
-          audioStream.put(data);
-        });
-      
-      
-        
-//         recogniser.start((err, service) => {
-//           if (err) {
-//             console.warn(err);
-//             return;
-//           }
-          
-//           console.log('🎤 Starting Speech Service')
-          
-//           service.on('recognition', recognitionEvent => {
-//             console.log(recognitionEvent)
-//           })
-//           service.on('error', (error) => {
-//             console.log(error);
-//           });
-//           service.on('close', () => {
-//             console.log('Speech API connection closed');
-//           });
-  
-//           recogniser.sendStream(audioStream);
-
-//         });
-      
-    });
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 });
 
-bot.connect();
+bot.login(process.env.DISCORD_BOT_TOKEN);
